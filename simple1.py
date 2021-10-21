4a5d6aeb7f8a171ffade81c20d9af8ecf3268a05
Johann Molano <jjdmolano@gmail.com>
2021-10-21 11:54:01 +0800

    Added simple pylint script test

---
+++ simple1.py
@@ -0,0 +1,16 @@
+'''
+A Very Simple Script to be tested by using the ff at the CLI:
+    pylint simple1.py -r y
+    -r y means to show the report
+'''
+
+def myfunc():
+    '''
+    A simple function
+    '''
+    first = 1
+    second = 2
+    print(first)
+    print(second)
+
+myfunc()
