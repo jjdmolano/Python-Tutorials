928adb9bccdef1767d03f7cec5c3ba9a79166cc8
Johann Molano <jjdmolano@gmail.com>
2021-11-08 19:46:38 +0800

    Fixed formatting

--- housing.py
+++ housing.py
@@ -4,12 +4,13 @@ pg.FAILSAFE = False
 
 status = False
 
+
 def main():
     while True:
         while status is True:
-            pg.press('`', presses = 3, interval = 0.7)
-            pg.press('left', interval = 0.1)
-            pg.press('`', interval = 0.7)
+            pg.press('`', presses=3, interval=0.7)
+            pg.press('left', interval=0.1)
+            pg.press('`', interval=0.7)
 
         def start():
             global status
@@ -21,6 +22,7 @@ def main():
 
         kb.add_hotkey('1', start)
         kb.add_hotkey('2', stop)
+
 
 if __name__ == '__main__':
     main()
--- pytester.py
+++ pytester.py
@@ -2,20 +2,28 @@ import pyautogui
 import time
 import sys
 from datetime import datetime
+
 pyautogui.FAILSAFE = False
 numMin = None
-if ((len(sys.argv)<2) or sys.argv[1].isalpha() or int(sys.argv[1])<1):
-    numMin = 5 # move curser after numMin minutes
-else:
-    numMin = int(sys.argv[1])
-while(True):
-    x=0
-    while(x<numMin):
-        time.sleep(60)
-        x+=1
-    for i in range(0,50):
-        pyautogui.moveTo(i*6+50,i*4)
-    pyautogui.moveTo(1,1)
-    for i in range(0,3):
-        pyautogui.press("shift")
-    print(f"Movement made at {datetime.now().time()}")
+
+
+def main():
+    if ((len(sys.argv) < 2) or sys.argv[1].isalpha() or int(sys.argv[1]) < 1):
+        numMin = 5  # move curser after numMin minutes
+    else:
+        numMin = int(sys.argv[1])
+    while True:
+        x = 0
+        while x < numMin:
+            time.sleep(60)
+            x += 1
+        for i in range(0, 50):
+            pyautogui.moveTo(i*6+50, i*4)
+        pyautogui.moveTo(1, 1)
+        for i in range(0, 3):
+            pyautogui.press('shift')
+        print(f'Movement made at {datetime.now().time()}')
+
+
+if __name__ == '__main__':
+    main()
