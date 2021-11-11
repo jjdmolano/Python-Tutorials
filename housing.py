f2cd99fe7bf25ad5659399c176d0b0da12f5674e
Johann Molano <jjdmolano@gmail.com>
2021-11-11 15:58:16 +0800

    Changed housing loop

--- housing.py
+++ housing.py
@@ -2,26 +2,30 @@ import pyautogui as pg
 import keyboard as kb
 pg.FAILSAFE = False
 
-status = False
+status = [False]
+
+
+def start():
+    status[0] = True
+
+
+def stop():
+    status[0] = False
+
+
+kb.add_hotkey('1', start)
+kb.add_hotkey('2', stop)
 
 
 def main():
     while True:
-        while status is True:
+        while status[0] is True:
             pg.press('`', presses=3, interval=0.7)
             pg.press('left', interval=0.1)
             pg.press('`', interval=0.7)
 
-        def start():
-            global status
-            status = True
-
-        def stop():
-            global status
-            status = False
-
-        kb.add_hotkey('1', start)
-        kb.add_hotkey('2', stop)
+            if status[0] is False:
+                break
 
 
 if __name__ == '__main__':
