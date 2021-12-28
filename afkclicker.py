d9f394b9a905e1a4bb9b91c555050e3ae2eb4a64
Johann Molano <jjdmolano@gmail.com>
2021-12-28 12:29:55 +0800

    Updated afkclicker

--- afkclicker.py
+++ afkclicker.py
@@ -1,11 +1,37 @@
 import pyautogui as pg
+import keyboard as kb
+import random as r
 pg.FAILSAFE = False
+
+status = [False]
+
+
+def start():
+    status[0] = True
+
+
+def stop():
+    status[0] = False
+
+
+kb.add_hotkey('1', start)
+kb.add_hotkey('2', stop)
 
 
 def main():
     while True:
-        pg.click(interval=5, button='left')
+        while status[0] is True:
+            random = r.randint(0,9)
+
+            if random in range(0,4):
+                pg.press('left', interval=1)
+            if random in range(5,9):
+                pg.press('right', interval=1)
+
+            if status[0] is False:
+                break
 
 
 if __name__ == '__main__':
     main()
+
